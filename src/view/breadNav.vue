ad4e189f94b4a7e85fff3921a5768724a934dc60
unknown <859158026@qq.com>
2017-08-04 16:33:20 +0800

    面包屑导航（未完善）

--- src/view/breadNav.vue
+++ src/view/breadNav.vue
@@ -1,24 +1,43 @@
 <template>
 	<div id="breadNav">
 	
-		<!-- <el-breadcrumb separator="/">
-		  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-		  <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-		  <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-		  <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-		</el-breadcrumb> -->
+		<el-breadcrumb separator="/">
+		 
+		  <el-breadcrumb-item v-for="(item,index) in navList" :key="item">
+		  	<router-link v-if='item.redirect==="noredirect"||index==navList.length-1' to="" class="no-redirect">{{item.name}}</router-link>
+			<router-link v-else :to="item.path">{{item.name}}</router-link>
+		  </el-breadcrumb-item>
+		  
+		</el-breadcrumb>
  	</div>
 </template>
 <script>
 	export default{
 		name:'#breadNav',
+		created() {
+		        this.getBreadcrumb()
+		      },
 		data() {
-	 	       return {
-	           	
+	 	     return {
+	           	navList:null
 	         };
 	      },
 		methods:{
-			
+			getBreadcrumb() {
+			  console.log('----', this.$route.matched)
+			  let matched = this.$route.matched.filter(item => item.name);
+			  const first = matched[0];
+			  if (first && (first.name !== '首页' || first.path !== '')) {
+			    matched = [{ name: '首页', path: '/' }].concat(matched)
+			  }
+			  console.log('面包屑:',matched);
+			  this.navList = matched;
+			}
+		},
+		watch: {
+		  $route() {
+		    this.getBreadcrumb();
+		  }
 		}
 		
 	}

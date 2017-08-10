ecd7a54b00a494c81d32ecdc59e8d798965176a5
ycAudrey <2516263634@qq.com>
2017-08-10 14:28:02 +0800

    Update login.vue

--- src/view/login.vue
+++ src/view/login.vue
@@ -119,14 +119,7 @@ export default {
 		font-size: 12px;
 		color: red;
 	}
-<<<<<<< HEAD
-	#login .el-form{
-		width: 100%!important;
-	}
-</style>
-=======
   #login .el-form{
 		width: 100%!important;
 	}
 </style>
->>>>>>> 827ed5399b5fdeda0d254af9b1f3ab09f3be637b
